b5776c08902a2a6dd7a9f731492e3e941fb74241
chLi <565353780@qq.com>
2024-05-08 21:21:25 +0800

    make auto trans dataset

--- auto_run.py
+++ auto_run.py
@@ -1,32 +1,46 @@
 import os
+from shutil import copyfile
 
 folder_path = "/home/chli/chLi/Dataset/SampledPcd/ShapeNet/03001627/"
+sample = '2048'
+alpha = '1.08'
+width_k = '7'
 
 pcd_filename_list = os.listdir(folder_path)
 pcd_filename_list.sort()
 
-start_idx = 0
-result_folder_path = './output/recon/123/'
-if os.path.exists(result_folder_path):
-    start_idx = len(os.listdir(result_folder_path))
+result_folder_path = './output/recon/sample_' + sample + '_k_' + width_k + '_min_0.0015_max_0.015_alpha_' + alpha + '_depth_min_1_depth_max_1/'
+save_folder_path = '/home/chli/chLi/Dataset/PGR_Recon_2048/ShapeNet/03001627/'
+os.makedirs(save_folder_path, exist_ok=True)
+
+solved_shape_names = os.listdir(save_folder_path)
 
 for i, pcd_filename in enumerate(pcd_filename_list):
-    if i < start_idx:
+    if pcd_filename[-4:] != '.npy':
+        continue
+
+    if pcd_filename in solved_shape_names:
         continue
 
     pcd_file_path = folder_path + pcd_filename
-    #pcd_file_path = '/home/chli/github/ASDF/ma-sh/output/normal_mash/103a0a413d4c3353a723872ad91e4ed1.ply'
-    #pcd_file_path = '../ma-sh/output/normal_mash/gs_30000itr.ply'
 
     cmd = (
         "python run_pgr.py "
         + pcd_file_path
-        + " --sample 2934"
-        + " --alpha 1.05"
-        + " --width_k 7"
+        + " --sample " + sample
+        + " --alpha " + alpha
+        + " --width_k " + width_k
     )
 
     print("start run shape[" + str(i) + "]:")
     print(cmd)
+
     os.system(cmd)
-    exit()
+
+    recon_mesh_file_path = result_folder_path + pcd_filename.split('.npy')[0] + '_sample-' + sample + '_recon_pgr.ply'
+    if os.path.exists(recon_mesh_file_path):
+        save_mesh_file_path = save_folder_path + pcd_filename.replace('.npy', '.ply')
+
+        copyfile(recon_mesh_file_path, save_mesh_file_path)
+
+    print('solved shape num:', i + 1)
