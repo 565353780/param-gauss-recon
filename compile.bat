d4f8ea768a074cc4b8862c379708687bfd34cfc9
chLi <565353780@qq.com>
2024-07-14 17:59:16 +0800

    remove \ error

--- compile.bat
+++ compile.bat
@@ -3,7 +3,5 @@ rmdir build
 mkdir build
 cd build
 
-cmake .. \
-	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-	-DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release
 make -j
