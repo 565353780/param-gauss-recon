d9c374f781fa1cd9239ec5f937d90e86133f4759
chLi <565353780@qq.com>
2024-09-13 23:55:15 +0800

    fix compile bug

--- compile.sh
+++ compile.sh
@@ -2,8 +2,7 @@ rm -rf build
 
 mkdir build
 cd build
-
-cmake .. \
-	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-	-DCMAKE_BUILD_TYPE=Release
+cmake \
+  -DCMAKE_PREFIX_PATH=$(python3 -c 'import torch;print(torch.utils.cmake_prefix_path)') \
+  ..
 make -j
