2a3bd0485d10db9ef464c5d4b2980a348043e14e
chLi <565353780@qq.com>
2024-11-23 02:17:49 +0800

    fix save normalize pcd file path format bug, must be xyz file format!

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
   PGRParams pgr_params;
   const std::string pcd_file_path = "/home/chli/Downloads/bunny.ply";
   const std::string save_normalize_pcd_file_path =
-      "/home/chli/Downloads/bunny_normalized.ply";
+      "/home/chli/Downloads/bunny_normalized.xyz";
   const std::string save_mesh_file_path = "/home/chli/Downloads/bunny_pgr.ply";
   const bool overwrite = true;
 
