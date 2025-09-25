8971a48457c8a932e3135cd0a2a3e256a1bbdefc
chLi <565353780@qq.com>
2025-09-25 14:35:51 +0800

    fix win path issue

--- param_gauss_recon/Config/path.py
+++ param_gauss_recon/Config/path.py
@@ -5,5 +5,5 @@ EXPORT_QUERY_EXE = "../param-gauss-recon/bin/PGRExportQuery"
 LOAD_QUERY_EXE = "../param-gauss-recon/bin/PGRLoadQuery"
 
 if platform.system() == "Windows":
-    EXPORT_QUERY_EXE += ".exe"
-    LOAD_QUERY_EXE += ".exe"
+    EXPORT_QUERY_EXE = EXPORT_QUERY_EXE.replace("/", "\\") + ".exe"
+    LOAD_QUERY_EXE = LOAD_QUERY_EXE.replace("/", "\\") + ".exe"
